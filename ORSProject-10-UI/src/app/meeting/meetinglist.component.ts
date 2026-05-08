7ac88dbfea8647c750811146e2ffd8827ce44683
Ankit Rawat <ankitrawat0988@gmail.com>
2026-05-08 16:22:24 +0530

    Update meetinglist.component.ts

--- ORSProject-10-UI/src/app/meeting/meetinglist.component.ts
+++ ORSProject-10-UI/src/app/meeting/meetinglist.component.ts
@@ -5,10 +5,6 @@ import { BaseListCtl } from '../base-list.component';
 import { ServiceLocatorService } from '../service-locator.service';
 import { ActivatedRoute } from '@angular/router';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a31e9f0dd079118b5421367151b7e7067175dc28
 @Component({
   selector: 'app-meeting-list',
   templateUrl: './meetinglist.component.html',
